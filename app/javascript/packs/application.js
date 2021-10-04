509b7991ffd7ac2c412b3fbeb786438ff61c0db5
Alexey Tamarov <lesha.tamarov@gmail.com>
2021-10-04 12:30:24 +0300

    added materializecss support

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "jquery"
+import "materialize-sprockets"
