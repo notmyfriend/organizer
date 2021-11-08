1473e7283bb3b3d0996d520fe062f49bb4f621db
Alexey Tamarov <lesha.tamarov@gmail.com>
2021-11-08 04:27:22 +0300

    added checkup for overlapping time intervals

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,5 +1,5 @@
 class TimeSlotsController < ApplicationController
-  before_action :get_organization_service
+  before_action :find_organization_service
 
   def index
     @time_slots = @organization_service.time_slots
@@ -24,18 +24,22 @@ class TimeSlotsController < ApplicationController
       time = current_day_start_time
 
       until (time + duration.minutes).to_s(:time) > end_time.to_s(:time) || (time + duration.minutes).day != time.day
-        time_slots_params_array << {
-          organization_service_id: org_service_id,
-          start_time: time,
-          end_time: time += duration.minutes,
-          status: :vacant
-        }
+        time_slot_start_time = time
+        time += duration.minutes
+        time_slot_end_time = time
+
+        unless overlaps?(TimeSlot.new(start_time: time_slot_start_time, end_time: time_slot_end_time))
+          time_slots_params_array << {
+            organization_service_id: org_service_id,
+            start_time: time_slot_start_time,
+            end_time: time_slot_end_time,
+            status: :vacant
+          }
+        end
       end
 
       current_day_start_time += 1.day
     end
-
-    # TODO: check if new time intervals don't intersect with existing intervals for this org_service
 
     begin
       TimeSlot.transaction do
@@ -48,20 +52,21 @@ class TimeSlotsController < ApplicationController
     end
   end
 
-  def edit
+  def edit  # на странице один datepicker и один timepicker
     @time_slot = @organization_service.time_slots.find(params[:id])
   end
 
   def update
     @time_slot = @organization_service.time_slots.find(params[:id])
 
-    # TODO: check if start and end time are in the same day 
-    # and don't intersect with existing intervals for this org_service
-
-    if @time_slot.update(time_slot_params)
-      redirect_to organization_service_time_slots_path
+    if !overlaps?(@time_slot)
+      if @time_slot.update(time_slot_params)
+        redirect_to organization_service_time_slots_path(@organization_service)
+      else
+        render :edit
+      end
     else
-      render :edit
+      render :edit  # можно выводить сообщение вроде "выбранный интервал времени пересекается с уже существующим"
     end
   end
 
@@ -69,14 +74,13 @@ class TimeSlotsController < ApplicationController
     @time_slot = TimeSlot.find(params[:id])
     @time_slot.destroy
 
-    redirect_to organization_service_time_slots_path
+    redirect_to organization_service_time_slots_path(@organization_service)
   end
 
   private
 
   def time_slot_params
     params.require(:time_slot).permit(
-      :organization_service_id,
       :start_time,
       :end_time,
       :time_slot_duration,
@@ -84,7 +88,7 @@ class TimeSlotsController < ApplicationController
     )
   end
 
-  def get_organization_service
+  def find_organization_service
     @organization_service = OrganizationService.find(params[:organization_service_id])
   end
 
@@ -96,5 +100,20 @@ class TimeSlotsController < ApplicationController
       time_slot_params["#{time}(4i)"].to_i,
       time_slot_params["#{time}(5i)"].to_i
     )
+  end
+
+  def overlaps?(time_interval)
+    overlappings = @organization_service.time_slots.where(
+      '
+      (end_time > ? and ? > end_time) or
+      (start_time > ? and start_time < ?) or
+      (start_time <= ? and end_time >= ?)
+      ',
+      time_interval.start_time, time_interval.end_time,
+      time_interval.start_time, time_interval.end_time,
+      time_interval.start_time, time_interval.end_time
+    )
+
+    !overlappings.empty?
   end
 end
